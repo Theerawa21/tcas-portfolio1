709b071eb2141f50f2f1bf55300028cb2c99b4b2
Theerawa21 <numpanoof@gmail.com>
2026-08-14 15:02:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.TCAS_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyvEgrNa4g3puQ5AasQdpqCN295_Gcr1f1j3gcVrEkx_VH_q0r1pr7LTqYdQiHfYqR2/exec',
+  appName: 'ระบบบันทึกข้อมูลผลงานนักเรียน',
+  schoolName: 'โรงเรียนเซนต์เทเรซา',
+  academicYear: '2569'
+};
